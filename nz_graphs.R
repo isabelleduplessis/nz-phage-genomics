bbdd50f0c18b12b3ccef45cfc39b0d88679ef03a
Isabelle Du Plessis <isabelleduplessis3@gmail.com>
2024-10-08 13:13:45 -0400

    Commit from RStudio

--- nz_graphs.R
+++ nz_graphs.R
@@ -1,51 +1,71 @@
 # New Zealand Viral Sequence Graphs
-# Code by: Isabelle Du Plessis, 2023
+# Code by: Isabelle Du Plessis, 2024 and Marian Dominguez-Mirazo, 2023
+# R version 4.3.2
 
-require(ggplot2)
-require(reshape2)
-require(gplots)
-require(vegan)
-library(readr)
-library(VennDiagram)
-require(tidyr)
-require(ggdendro)
-require(ggvenn)
-require(ggVennDiagram)
+require(ggplot2) # v3.4.4
+require(reshape2) # v1.4.4
+require(readr) # v2.1.4
+require(vegan) # v2.6.8
+require(factoextra) # v1.0.7
+require(dplyr) # v1.1.4
+require(ggvenn) # v0.1.10
 
 
+### Load Data ###
 
-# sequence length distribution
-nzseqlens <- read.table("~/phage/NZ/abundance/nzseqlens.txt", quote="\"", comment.char="")
+# Get path
+path = getwd() # Set path
 
-# include all sequences
-hist(nzseqlens$V1, breaks=max(nzseqlens$V1)/100)
+# Load sequence lengths
+nzseqlens = read.table(paste0(path, "/data/nzseqlens.txt"), quote="\"", comment.char="")
 
-# include only sequences smaller than 50,000 bp
-hist(nzseqlens$V1[nzseqlens$V1<50000], breaks=50000/100, xlab = "Sequence Length (bin size = 100)", main = "Sequence Length Distribution") 
-# still need to try to add cut ahead axis
+# Load sample metadata
+metadata = read.csv(paste0(path, "/data/metadata.csv"))
 
-
-
-#load sample metadata
-virome_with_bottle <- read.csv("~/phage/NZ/abundance/virome_with_bottle.csv")
-metadata <- data.frame("Sample"=virome_with_bottle$sample_id, "Depth"=virome_with_bottle$depth_numeric, "Cycle"=virome_with_bottle$Cycle, "Temperature"=virome_with_bottle$Temp1...C., "Salinity"=virome_with_bottle$Salinity1.PSS.78., "Oxygen"=virome_with_bottle$Oxygen1..mmol.kg., "Water Mass"=virome_with_bottle$water_mode)
-
+# Format metadata
+metadata = data.frame("Sample"=metadata$sample_id, "Depth"=metadata$depth_numeric, "Cycle"=metadata$Cycle, "Temperature"=metadata$Temp1...C., "Salinity"=metadata$Salinity1.PSS.78., "Oxygen"=metadata$Oxygen1..mmol.kg., "Water Mass"=metadata$water_mode)
 metadata$Sample=parse_number(metadata$Sample)
 metadata=metadata[metadata$Sample>=3,]
+
+# Get average values across samples
 newmetadata=data.frame(aggregate(list(metadata$Depth, metadata$Cycle, metadata$Temperature, metadata$Salinity, metadata$Oxygen), by=list(metadata$Sample), FUN=mean))
-newmetadata$Water="Subtropical"
-colnames(newmetadata)=c("Sample", "Depth", "Cycle", "Avg Temperature", "Avg Salinity", "Avg Oxygen", "Water")
+colnames(newmetadata)=c("Sample", "Depth", "Cycle", "Avg Temperature", "Avg Salinity", "Avg Oxygen")
+
+# Relabel water mass types
+newmetadata$Water="Subtropical" 
 newmetadata$Water[newmetadata$Cycle!=3]="Subantarctic"
 
+# Load coverage data
+votus_cov75thres = read.delim(paste0(path, "/data/nz_votus_cov75thres.txt"), header=FALSE, comment.char="#")
+colnames(votus_cov75thres) = c("sample","votus","coverage","meandepth","rpkm")
 
-# proportion of reads that mapped to each sample
-totalreads_mapped <- read.delim("~/phage/NZ/abundance/totalreads_mapped.csv", header=FALSE)
-totalreads <- read.csv("~/phage/NZ/abundance/totalreads.csv", header=FALSE)
-totalreads = totalreads[order(totalreads$V1),]
-totalreads=totalreads[parse_number(totalreads$V1)>=3,]
-viralreads = totalreads_mapped
-viralreads = viralreads[order(viralreads$V1),]
+# Load mapping data
+viralreads = read.delim(paste0(path,"/data/totalreads_mapped.tsv"), header=FALSE)
+totalreads = read.csv(paste0(path, "/data/totalreads.csv"), header=FALSE)
+totalreads=totalreads[parse_number(totalreads$V1)>=3,] # Only include samples 3-22 because they have water type labels
 viralreads=viralreads[parse_number(viralreads$V1)>=3,]
+
+
+##### PLOTS #####
+
+
+### Sequence Length Distribution
+
+# Include only sequences smaller than 50,000 bp - average phage genome length
+trimmedseqlens = data.frame("BasePairs" = nzseqlens$V1[nzseqlens$V1<50000])
+trimmedseqlens$BasePairs = trimmedseqlens$BasePairs/1000
+ggplot(data = trimmedseqlens, aes(x=BasePairs)) +
+  geom_histogram(binwidth = .5, color="black", fill="lightblue") +
+  xlab("Sequence Length (kBP)") +
+  ylab("Number of Sequences") +
+  theme_test() +
+  ggtitle("Viral Sequence Length Distribution") +
+  theme(plot.title = element_text(hjust = .5)) +
+  geom_vline(aes(xintercept=mean(BasePairs)), color="black", linetype="dashed", linewidth=.7) +
+  annotate("text", x=12, y=1700, label=paste("Mean Sequence \nLength (kBP) = ", round(mean(trimmedseqlens$BasePairs),2)), size=3, hjust = 0)
+
+
+### Proportion of Viral Reads
 
 propviralreads = data.frame("Sample"=parse_number(viralreads$V1),"Viral"=viralreads$V2/totalreads$V2, "Water"=newmetadata$Water)
 propviralreads$Water = factor(propviralreads$Water,levels = c("Subtropical", "Subantarctic"))
@@ -55,16 +75,12 @@ ggplot(data=propviralreads, aes(y =Viral, x=Water, color=Water)) +
   geom_point() +
   theme_test() +
   xlab("Sample") +
-  ylab("Proportion of sample reads that map to viral sequence")
+  ylab("Proportion of Reads Mapped") +
+  ggtitle("Proportion of Sample Reads Mapped to Viral Sequences")
+  
 
 
-
-############################
-## Total RPKM 
-
-votus_cov75thres <- read.delim("~/phage/NZ/abundance/nz_votus_cov75thres_afterincreasing.txt", header=FALSE, comment.char="#")
-#votus_cov75thres <- read.delim("~/Desktop/nz_votus_cov75thres_beforeincreasing.txt", header=FALSE, comment.char="#")
-colnames(votus_cov75thres) = c("sample","votus","coverage","meandepth","rpkm")
+### Total RPKM 
 
 #Plot total rpkm per sample, grouped by metadata
 tmp = as.numeric(as.character(votus_cov75thres$rpkm))
@@ -76,8 +92,6 @@ tmp=tmp[order(tmp$sample),]
 tmp = cbind(tmp, newmetadata[,2:7])
 tmp$Water = factor(tmp$Water,levels = c("Subtropical", "Subantarctic"))
 tmp$Cycle=as.character(tmp$Cycle)
-
-
 rpkmpersample = tmp
 
 ggplot(data=tmp,aes(Water,rpkm,color=Water)) +
@@ -89,7 +103,7 @@ ggplot(data=tmp,aes(Water,rpkm,color=Water)) +
   theme(axis.text.x=element_text(size=13), axis.text.y=element_text(size=13), legend.text = element_text(size=13), axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
 
 
-# rpkm vs salinity colored by cycle
+# RPKM vs salinity colored by cycle
 ggplot(data=tmp,aes(`Avg Salinity`,rpkm,color=Cycle)) +
   #geom_boxplot(color="black") +
   geom_point(size=2.5) +
@@ -98,7 +112,7 @@ ggplot(data=tmp,aes(`Avg Salinity`,rpkm,color=Cycle)) +
   ylab("Total RPKM") +
   theme(axis.text.x=element_text(size=13), axis.text.y=element_text(size=13), legend.text = element_text(size=13), axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
 
-# rpkm vs temperature colored by cycle
+# RPKM vs temperature colored by cycle
 ggplot(data=tmp,aes(`Avg Temperature`,rpkm,color=Cycle)) +
   #geom_boxplot(color="black") +
   geom_point(size=2.5) +
@@ -108,8 +122,7 @@ ggplot(data=tmp,aes(`Avg Temperature`,rpkm,color=Cycle)) +
   theme(axis.text.x=element_text(size=13), axis.text.y=element_text(size=13), legend.text = element_text(size=13), axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
 
 
-############################
-#Number of votus per sample
+### Number of VOTUS per sample
 tmp = melt(table(votus_cov75thres$sample))
 colnames(tmp) = c("sample","votus")
 tmp$sample=parse_number(as.character(tmp$sample))
@@ -119,13 +132,8 @@ tmp = cbind(tmp, newmetadata[,2:7])
 tmp$Water = factor(tmp$Water,levels = c("Subtropical", "Subantarctic"))
 tmp$Cycle=as.character(tmp$Cycle)
 
-
-
 newtable = data.frame(rpkmpersample$sample, rpkmpersample$rpkm, tmp$votus, rpkmpersample$Depth, rpkmpersample$Cycle)
 colnames(newtable) = c("Sample", "RPKM", "vOTUs", "Depth", "Cycle")
-
-
-
 
 ggplot(data=tmp,aes(Water,votus,color=Water)) +
   geom_point(size=2.5) +
@@ -151,9 +159,7 @@ ggplot(data=tmp,aes(`Avg Temperature`,votus,color=Cycle)) +
   theme(axis.text.x=element_text(size=13), axis.text.y=element_text(size=13), legend.text = element_text(size=13), axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
 
 
-
-############################
-#### Shannon diversity for full mapping
+### Shannon diversity for full mapping
 df = data.frame(matrix(ncol = 2, nrow = 20))
 colnames(df) = c("sample","diversity")
 
@@ -178,22 +184,24 @@ ggplot(data=df,aes(Water,diversity,color=Water)) +
   geom_point(size=2.5) +
   theme_test() + 
   xlab("") + 
-  ylab("Shannon Diversity index")
+  ylab("Shannon Diversity index") +
+  theme(axis.text.x=element_text(size=13), axis.text.y=element_text(size=13), legend.text = element_text(size=13), axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
 
 
 
-## Cumulative vOTUs
+### Cumulative number of vOTUs per sample
+
 
 perms = 100
 le_tmp = table(votus_cov75thres[,1:2])
 main = matrix(0,nrow=perms,ncol=20)
 for(perm in 1:perms){
-  tots = rep(0,7890)
+  tots = rep(0,18490)
   tmp = le_tmp[sample(1:20),]
   for(i in 1:20){
     tmp2= tmp[i,] - tots
     tmp2[tmp2<0]=0
-    main[perm,i] = sum(tmp2) #change the 1
+    main[perm,i] = sum(tmp2)
     tots = tots + tmp2
     tots[tots>1]=1
   }
@@ -210,8 +218,7 @@ ggplot(df,aes(Var2,value,group='Var1')) +
   theme_test()
 
 
-
-# Rank order graph
+### Rank Order Plot
 my_table = votus_cov75thres[,c(1,2,5)]
 for(i in 3:22){
   my_sample = i
@@ -225,51 +232,43 @@ for(i in 3:22){
   assign(nam, tmpdf)
 }
 
-
 totaldf = rbind(df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14, df15, df16, df17, df18, df19, df20, df21, df22)
 totaldf$sample = as.character(totaldf$sample)
 totaldf$order=totaldf$order/max(totaldf$order) # normalize x axis zero to 1 percentage
 
 ggplot(totaldf,aes(order,rpkm,color=sample)) + 
   geom_line() +
-  theme_test() 
+  theme_test() +
+  xlab("Order Proportion") +
+  ylab("RPKM")
 
 
-
-#############################
-# NMDS plot
+### NMDS Plot
 votus_cov75thres=votus_cov75thres[parse_number(votus_cov75thres$sample)>=3,]
 otherdf=votus_cov75thres[,c(1,2,5)]
 rpkm = dcast(otherdf, sample ~ votus)
-rpkm[is.na(rpkm)] <- 0
+rpkm[is.na(rpkm)] = 0
 rpkm$sample=parse_number(rpkm$sample)
-
-rpkm=rpkm[rpkm$sample!=19]
-
+rpkm=rpkm[rpkm$sample!=19] # Exclude outlier sample
 m_rpkm = as.matrix(rpkm)
-
 k5 = kmeans(m_rpkm, 5, nstart=25)
 fviz_cluster(k5, m_rpkm, ggtheme = theme_test(), geom = "point")
 
 nmds = metaMDS(m_rpkm, distance="bray")
-data.scores = as.data.frame(scores(nmds))
+data.scores = data.frame(scores(nmds)$sites)
 data.scores$Sample = rpkm$sample
 data.scores=data.scores[order(data.scores$Sample),]
 tmp = as.numeric(as.character(votus_cov75thres$rpkm[votus_cov75thres$sample!=19]))
-#tmp = as.numeric(as.character(votus_cov75thres$rpkm))
 tmp = aggregate(tmp,list(sample=votus_cov75thres$sample[votus_cov75thres$sample!=19]),sum)
-#tmp = aggregate(tmp,list(sample=votus_cov75thres$sample),sum)
 colnames(tmp)=c("sample","rpkm")
 tmp$sample=parse_number(tmp$sample)
 tmp=tmp[tmp$sample>=3,]
 tmp=tmp[order(tmp$sample),]
-#newmetadata=newmetadata[newmetadata$sample!=19,]
 tmp = cbind(tmp, newmetadata[ ,2:7])
 tmp$Water = factor(tmp$Water,levels = c("Subtropical", "Subantarctic"))
 data.scores=data.scores[data.scores$Sample>=3,]
 data.scores$Water=tmp$Water
 data.scores$Cycle=tmp$Cycle
-
 data.scores=data.scores[data.scores$Sample!=19,]
 
 ggplot(data.scores, aes(x=NMDS1, y=NMDS2, shape = as.character(Cycle), color = Water)) +
@@ -278,74 +277,7 @@ ggplot(data.scores, aes(x=NMDS1, y=NMDS2, shape = as.character(Cycle), color = W
   theme_test()
 
 
-
-# cluster by presence or absence
-votus_cov75thres=votus_cov75thres[parse_number(votus_cov75thres$sample)>=3,]
-otherdf=votus_cov75thres[,c(1,2)]
-otherdf$value = 1
-rpkm = dcast(otherdf, sample ~ votus)
-rpkm[is.na(rpkm)] <- 0
-rpkm$sample=parse_number(rpkm$sample)
-
-
-
-
-
-library(ggpubr)
-library(factoextra)
-
-m_rpkm = as.matrix(rpkm)
-
-k5 = kmeans(m_rpkm, 5, nstart=25)
-fviz_cluster(k5, m_rpkm, ggtheme = theme_test(), geom = "point")
-
-## heatmap - includes sample 19
-votus_cov75thres=votus_cov75thres[parse_number(votus_cov75thres$sample)>=3,]
-otherdf=votus_cov75thres[,c(1,2,5)]
-rpkm = dcast(otherdf, sample ~ votus) 
-rpkm[is.na(rpkm)] <- 0
-rpkm$sample=parse_number(rpkm$sample)
-rpkm = rpkm[order(rpkm$sample),]
-
-rpkm=rpkm[,1:150]
-# for each sample, sum all the viruses in the sample and divide each by total number to normalize
-for(i in 1:20){
-  #totalsum = sum(rpkm[i,-1])
-  rpkm[i,-1]=rpkm[i,-1]/max(rpkm[i,-1])
-}
-
-
-
-h = hclust(dist(rpkm[,-1]))
-h$order
-plot(h)
-
-ord = c(h$order)
-
-dendro <- as.dendrogram(h)
-
-# Create dendro
-dendro_plot <- ggdendrogram(data = dendro, rotate = TRUE)
-
-# Preview the plot
-print(dendro_plot)
-
-long <- pivot_longer(data = rpkm, cols=-c(sample))
-
-long = long[long$sample!=19,]
-
-
-ggplot(data = long, aes(x = name, y = as.character(sample))) +
-  geom_tile(aes(fill = value)) +
-  scale_fill_gradient2() +
-  theme(axis.text.y = element_text(size = 6)) +
-  scale_y_discrete(limits=c("3", "4", "19", "20", "17", "8", "18", "5", "6", "7", "11", "12", "16", "9", "10", "15", "13", "14")) +
-  scale_x_discrete(labels=NULL) +
-  xlab("vOTUs") +
-  ylab("Sample")
-
-
-## venn diagram
+### Venn diagram
 
 newdf = votus_cov75thres[,c(1,2)]
 
@@ -375,44 +307,11 @@ plotcycles[is.na(plotcycles)] <- 0
 
 colnames(plotcycles) = c("votus", "cycle1", "cycle2", "cycle3", "cycle4", "cycle5")
 
-
+# Can only plot 4 categories at a time, find alternate way of displaying
 ggplot(plotcycles, aes(A = as.logical(cycle1), B = as.logical(cycle2), C = as.logical(cycle3), D = as.logical(cycle4), E = as.logical((cycle5)))) +
   geom_venn(show_percentage = FALSE, set_names = c("Cycle 1", "Cycle 2", "Cycle 3", "Cycle 4", "Cycle 5")) + 
   theme_test() +
   theme(text = element_text(size = 15), panel.border = element_blank()) +
   scale_x_continuous(NULL, NULL, NULL) +
   scale_y_continuous(NULL, NULL, NULL)
-
-####
-
-
-
-
-
-
-
-
-library(RColorBrewer)
-myCol <- brewer.pal(9, "YlGnBu")
-
-
-
-
-myCol = c("#41B6C4", "#FC8D62", "#A6D854", "#E78AC3", "#FFD92F")
-
-
-venn.diagram(x=list(unique(c1$votus), unique(c2$votus), unique(c3$votus), unique(c4$votus), unique(c5$votus)), 
-             category.names = c("Cycle 1", "Cycle 2", "Cycle 3", "Cycle 4", "Cycle 5"), 
-             filename = 'cycles.png',
-             output=TRUE,
-             #lty = 'blank',
-             fill = myCol,
-             width = 4000, 
-             cat.fontface = "bold",
-             lwd = 2,
-             fontfamily = "sans",
-             cat.fontfamily = "sans",
-             cat.pos = c(0,0,0,0,0),
-             cat.dist = c(.2, .2, -.15, -.2,.2),
-             cex = .9)
 
